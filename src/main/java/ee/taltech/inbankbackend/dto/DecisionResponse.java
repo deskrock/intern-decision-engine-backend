f7154fe1d86c9e33b9355970f5fcb1d0d641f3e2
Tea Tosun <teadulellari@gmail.com>
2024-04-15 01:51:19 +0300

    Removing the Component annotation from DecisionResponse and adding the necessary lombok annotations

--- src/main/java/ee/taltech/inbankbackend/dto/DecisionResponse.java
+++ src/main/java/ee/taltech/inbankbackend/dto/DecisionResponse.java
@@ -1,15 +1,15 @@
 package ee.taltech.inbankbackend.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.stereotype.Component;
 
 /**
  * Holds the response data of the REST endpoint.
  */
 @Getter
 @Setter
-@Component
+@AllArgsConstructor
 public class DecisionResponse {
     private Integer loanAmount;
     private Integer loanPeriod;
